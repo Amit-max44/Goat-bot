28d47ff41ba5b928263da61fb0e2ae5e0c8cfbbd
Amit Max <205285368+Amit-max44@users.noreply.github.com>
2025-04-01 15:16:02 +0600

    Update fun2.js

--- scripts/cmds/fun2.js
+++ scripts/cmds/fun2.js
@@ -26,6 +26,14 @@ module.exports = {
     const driveLinks = [
       "https://drive.google.com/file/d/13t3UibYYbEUWG7EoWT7LGbepmM6mmM9g/view?usp=drivesdk",
       "https://drive.google.com/file/d/12wXF3ir_QHPLTT3MbG0IGC8qrPloO8Sa/view?usp=drivesdk",
+      "https://drive.google.com/file/d/14Y3xgv4QtRXI-Rp2FWOE02exn64VmZZ3/view?usp=drivesdk",
+      "https://drive.google.com/file/d/14seg6wY3XOkugrGRS6Cb8ap42tA3uqVM/view?usp=drivesdk",
+      "https://drive.google.com/file/d/13CLkzlJ4O1AJVF-zOCMYJSdRP89kJ8TD/view?usp=drivesdk",
+      "https://drive.google.com/file/d/131eK1tUWwDJQVRukVkUlpNp947lScINU/view?usp=drivesdk",
+      "https://drive.google.com/file/d/13D6rGpz3TMcN1TGHW-hi75QJbMAKexU4/view?usp=drivesdk",
+      "https://drive.google.com/file/d/13GVrsWVqChcsvp5BYaW1zAc0gSK6BvcA/view?usp=drivesdk",
+      "https://drive.google.com/file/d/136EKtYvefiZe12zyZBGFvrGPyZ8Jt5ri/view?usp=drivesdk",
+      "https://drive.google.com/file/d/13z7fXXLVFfKyaEbRy-sIWYo0GC1xio6K/view?usp=drivesdk",
     
 
 
